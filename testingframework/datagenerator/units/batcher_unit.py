e47447e9d0bf3bc3cd484f1663656f9342c8534f
Peter Vasilyev <p.vasilyev@inbox.ru>
2019-10-22 21:17:44 +0300

    Implemented BatcherUnit and PreprocessorUnit classes

--- testingframework/datagenerator/units/batcher_unit.py
+++ testingframework/datagenerator/units/batcher_unit.py
@@ -1,19 +1,28 @@
 from typing import List, Union
 
-from testingframework.datagenerator.units.unit import Unit
+from testingframework.datagenerator.units.unit import Unit, FileNameType
+
+BatchType = List[FileNameType]
 
 
 class BatcherUnit(Unit):
-    def __init__(self, batch_size: int, output_list: list):
+    def __init__(self, batch_size: int, output_list: List[BatchType]):
         self.batch_size = batch_size
         self.output_list = output_list
     # end of '__init__' function
 
-    def process(self, file_names: List[Union[str, List[str]]]) -> None:
+    def process(self, file_names: List[FileNameType]) -> None:
         """Separate file names into batches of 'batch_size' length and
         append those batches to 'output_list'.
 
         Args:
             file_names (list): Input list of strings or lists of strings.
         """
-        pass
+        batch = []
+        for name in file_names:
+            batch.append(name)
+            if len(batch) == self.batch_size:
+                self.output_list.append(batch)
+                batch = []
+        if len(batch) != 0:
+            self.output_list.append(batch)
--- testingframework/datagenerator/units/preprocessor_unit.py
+++ testingframework/datagenerator/units/preprocessor_unit.py
@@ -1,13 +1,45 @@
 from typing import List, Union
+import pathlib as pth
 
-from testingframework.datagenerator.units.unit import Unit
+from testingframework.datagenerator.units.unit import Unit, FileNameType
 
 
 class PreprocessorUnit(Unit):
-    def process(self, file_names: List[Union[str, List[str]]]) -> None:
+    def process(self, file_names: List[FileNameType]) -> None:
         """Unravel directory names into contained file names. Sort file names.
 
         Args:
             file_names (list): Input list of strings or lists of strings.
         """
-        pass
+        # unravel directories
+        for name in file_names:
+            path = pth.Path(name)
+
+            # check path exists
+            if not path.exists():
+                file_names.remove(name)
+                continue
+
+            # check if directory
+            if path.is_dir():
+                # remove directory from 'file_names'
+                file_names.remove(name)
+
+                # find contained files with unique name stems
+                files = []
+                file_stems = []
+                for child in path.iterdir():
+                    if not child.is_file():
+                        continue
+                    # add if unique name stem
+                    if child.stem not in file_stems:
+                        file_stems.append(child.stem)
+                        files.append(child)
+
+                # add contained files to 'file_names'
+                file_names.extend([str(file) for file in files])
+
+        # sort 'file_names'
+        file_names.sort()
+
+        self.next_unit.process(file_names)
